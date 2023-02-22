32a5a99dbaf5cc021c4711f4326a7d21dc517019
wellington36 <wellington.71319@gmail.com>
2023-02-22 18:43:37 -0300

    Add G transformation

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import math
-from acceleration import Aitken_tranform, Richardson_transform, Epsilon_transfom
+from acceleration import Aitken_tranform, Richardson_transform, Epsilon_transfom, G_transform
 
 def square_serie(n: int) -> list:
-    serie = [0]
+    serie = [1.0]
 
-    for i in range(1, n):
+    for i in range(2, n+1):
         serie.append(serie[-1] + 1/(i)**2)
     
     return serie
@@ -12,7 +12,7 @@ def square_serie(n: int) -> list:
 if __name__ == "__main__":
     for i in range(1, 6):
         initial_serie = abs(math.pi**2 / 6 - square_serie(10**i)[-1])
-        acceration_serie = abs(math.pi**2 / 6 - Epsilon_transfom(square_serie(10**i), 1)[-1])
+        acceration_serie = abs(math.pi**2 / 6 - Aitken_tranform(square_serie(10**i))[-1])
 
         print(f"Error in initial serie with n={10**i}: {initial_serie}")
         print(f"Errro in acceleration serie with n={10**i}: {acceration_serie}")
