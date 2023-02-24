95450d53dc9f4721e449ab751af253d41bd43fdf
Wellington José Leite da Silva <wellington.71319@gmail.com>
2023-02-24 16:14:49 -0300

    Typo series

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 import math
 from acceleration import Aitken_tranform, Richardson_transform, Epsilon_transfom, G_transform
 
-def square_serie(n: int) -> list:
-    serie = [1.0]
+def square_series(n: int) -> list:
+    series = [1.0]
 
     for i in range(2, n+1):
-        serie.append(serie[-1] + 1/(i)**2)
+        series.append(series[-1] + 1/(i)**2)
     
-    return serie
+    return series
 
 if __name__ == "__main__":
     for i in range(1, 6):
-        initial_serie = abs(math.pi**2 / 6 - square_serie(10**i)[-1])
-        acceration_serie = abs(math.pi**2 / 6 - Aitken_tranform(square_serie(10**i))[-1])
+        initial_series = abs(math.pi**2 / 6 - square_series(10**i)[-1])
+        acceration_series = abs(math.pi**2 / 6 - Aitken_tranform(square_series(10**i))[-1])
 
-        print(f"Error in initial serie with n={10**i}: {initial_serie}")
-        print(f"Errro in acceleration serie with n={10**i}: {acceration_serie}")
+        print(f"Error in initial series with n={10**i}: {initial_series}")
+        print(f"Errro in acceleration series with n={10**i}: {acceration_series}")
         print("\n")
 
-    print(Epsilon_transfom(square_serie(10)))
+    print(Epsilon_transfom(square_series(10)))
