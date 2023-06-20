8bfc615ff5676cdd38856e09e91d3b8cdfd03110
wellington36 <wellington.71319@gmail.com>
2023-06-20 15:38:11 -0300

    Add more tests

--- tests.py
+++ tests.py
@@ -1,7 +1,23 @@
 from src.acceleration import acceleration, no_transform, Richardson_transform, Aitken_transform, Epsilon_transform, G_transform, partial_sum
+import numpy as np
 
 def basel_series(n: int):
     return 1/(n)**2
+
+def equivalence(a, b, epsilon=0.0001):
+    return abs(a - b) < epsilon
+
+#################### TESTS ####################
+
+def test_partial_sum():
+    array = partial_sum(basel_series, 4)
+
+    assert type(array) == np.ndarray
+    assert len(array) == 4
+    assert equivalence(array[0], 1)
+    assert equivalence(array[1], 5/4)
+    assert equivalence(array[2], 49/36)
+    assert equivalence(array[3], 205/144)
 
 def test_len_transformations():
     assert len(no_transform(partial_sum(basel_series, 10))) == 10
@@ -10,6 +26,19 @@ def test_len_transformations():
     assert len(Epsilon_transform(partial_sum(basel_series, 10))) == 8
     assert len(G_transform(partial_sum(basel_series, 10))) == 7
 
+def test_simple_acceleration():
+    n, acel = acceleration(basel_series, no_transform, 0.1)
+    assert type(n) == int
+    assert type(acel) == np.ndarray
+    assert len(acel) == 10
+
+    n, acel = acceleration(basel_series, no_transform, 0.01)
+    assert type(n) == int
+    assert type(acel) == np.ndarray
+    assert len(acel) == 100
+
 
 if __name__ == '__main__':
+    test_partial_sum()
     test_len_transformations()
+    test_simple_acceleration()
