a71d7beaa4f9231b3ba84eac6221e5f3d1a3c154
wellington36 <wellington.71319@gmail.com>
2023-06-20 19:52:49 -0300

    Initial structure to mpmath

--- acceleration/configuration.py
+++ acceleration/configuration.py
@@ -2,8 +2,8 @@ import numpy as np
 from mpmath import mp
 
 # SET DATA TYPE
-DT = np.dtype('float128') # numpy precision
-mp.dps = 100 # mpmath precision
+DT = np.dtype('float128')   # numpy precision
+mp.dps = 100                # mpmath precision
 
 # SET zeta(1.2), PI, log 2, limit of slow series VALUE APPROXIMATION (~100)
 Z = 5.591582441177750776536563193423143277629903241802331099473728250024897906802651077991103320801610144
