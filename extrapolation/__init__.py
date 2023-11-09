4cd2b57b9389934429468ab05d0e9ed23d579fc2
Wellington36 <wellington.71319@gmail.com>
2023-11-09 17:27:49 -0300

    Add transformations in the library

--- extrapolation/__init__.py
+++ extrapolation/__init__.py
@@ -1,2 +1,15 @@
-from .esum import acelsum, esum
+from .esum import (
+    esum,
+    acelsum,
+    partial_sum_list,
+    partial_sum_mp,
+    no_transform,
+    Aitken_transform,
+    Richardson_transform,
+    Epsilon_transform,
+    G_transform,
+    Levin_t_transform,
+    Levin_u_transform,
+    Levin_v_transform
+) 
 from .utils import LogNumber, create_lognumber
